ed8463ea828ed1be18cd65cbc6a871e827590c41
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-03-09 05:07:19 +0000

    Initial commit

---
+++ AbbreviatedNumbers.Tests/Tests.cs
@@ -0,0 +1,44 @@
+﻿namespace AbbreviatedNumbers.Tests;
+
+public class Tests {
+    [Fact]
+    public void SmallIntegerTest() {
+        Assert.Equal("1", 1.AbbreviateNumber());
+        Assert.Equal("2", 2.AbbreviateNumber());
+    }
+    [Fact]
+    public void SmallFloatTest() {
+        Assert.Equal("1", 1.2345.AbbreviateNumber());
+        Assert.Equal("2", 1.5345.AbbreviateNumber());
+    }
+    [Fact]
+    public void SmallFloatDecimalsTest() {
+        Assert.Equal("1.23", 1.2345.AbbreviateNumber(2));
+        Assert.Equal("1.53", 1.5345.AbbreviateNumber(2));
+    }
+    [Fact]
+    public void LargeIntegerTest() {
+        Assert.Equal("1K", 1234.AbbreviateNumber());
+        Assert.Equal("6K", 5678.AbbreviateNumber());
+    }
+    [Fact]
+    public void NegativeLargeIntegerDecimalsTest() {
+        Assert.Equal("-1K", (-1234).AbbreviateNumber());
+        Assert.Equal("-6K", (-5678).AbbreviateNumber());
+    }
+    [Fact]
+    public void LargeFloatTest() {
+        Assert.Equal("1K", 1234.56.AbbreviateNumber());
+        Assert.Equal("6K", 5678.56.AbbreviateNumber());
+    }
+    [Fact]
+    public void LargeFloatDecimalsTest() {
+        Assert.Equal("1.2K", 1234.56.AbbreviateNumber(1));
+        Assert.Equal("5.68K", 5678.56.AbbreviateNumber(2));
+    }
+    [Fact]
+    public void NegativeLargeFloatDecimalsTest() {
+        Assert.Equal("-1.2K", (-1234.56).AbbreviateNumber(1));
+        Assert.Equal("-5.68K", (-5678.56).AbbreviateNumber(2));
+    }
+}
---
+++ AbbreviatedNumbers/NumberAbbreviator.cs
@@ -0,0 +1,79 @@
+using System.Collections.Frozen;
+using System.Numerics;
+
+namespace AbbreviatedNumbers;
+
+/// <summary>
+/// Methods to abbreviate numbers.
+/// </summary>
+public static class NumberAbbreviator {
+    /// <summary>
+    /// Standard number abbreviations.
+    /// </summary>
+    private static readonly FrozenDictionary<BigInteger, string> DefaultAbbreviations = new Dictionary<BigInteger, string> {
+        [1_000] = "K",
+        [1_000_000] = "M",
+        [1_000_000_000] = "B",
+        [1_000_000_000_000] = "T",
+    }.ToFrozenDictionary();
+
+    /// <summary>
+    /// Abbreviates the number to the nearest abbreviation.<br/>
+    /// For example:
+    /// <code>
+    /// AbbreviateNumber(-5678) // -6K
+    /// AbbreviateNumber(1234.56, 1) // 1.2K
+    /// </code>
+    /// </summary>
+    public static string AbbreviateNumber<TValue, TAbbreviation>(this TValue Value, int DecimalPlaces, IReadOnlyDictionary<TAbbreviation, string> Abbreviations, IFormatProvider? Provider = null)
+        where TValue : INumberBase<TValue>, IComparisonOperators<TValue, TValue, bool>
+        where TAbbreviation : INumberBase<TAbbreviation>
+    {
+        // Create format string
+        string Format = "0." + new string('#', DecimalPlaces);
+        // Get positive value
+        TValue AbsoluteValue = TValue.Abs(Value);
+
+        // Find largest abbreviation
+        foreach ((TAbbreviation Divisor, string Abbreviation) in Abbreviations.OrderByDescending(Pair => Pair.Key)) {
+            // Convert divisor to value type
+            TValue DivisorAsTValue = TValue.CreateSaturating(Divisor);
+
+            // Ensure value is at least divisor
+            if (AbsoluteValue < DivisorAsTValue) {
+                continue;
+            }
+
+            // Divide value by abbreviation divisor
+            TValue AbbreviatedValue = Value / DivisorAsTValue;
+
+            // Check if integer division was performed
+            if (AbbreviatedValue * DivisorAsTValue != Value) {
+                // Round integer division (https://stackoverflow.com/a/41078274)
+                TValue Two = TValue.One + TValue.One;
+                TValue PreQuotient = Value * Two / DivisorAsTValue;
+                TValue Offset = TValue.IsNegative(PreQuotient) ? -TValue.One : TValue.One;
+                AbbreviatedValue = (PreQuotient + Offset) / Two;
+            }
+
+            // Stringify abbreviated value
+            return AbbreviatedValue.ToString(Format, Provider) + Abbreviation;
+        }
+
+        // Stringify small value
+        return Value.ToString(Format, Provider);
+    }
+    /// <inheritdoc cref="AbbreviateNumber{TValue, TAbbreviation}(TValue, int, IReadOnlyDictionary{TAbbreviation, string}, IFormatProvider?)"/>
+    public static string AbbreviateNumber<TValue, TInteger>(this TValue Value, IReadOnlyDictionary<TInteger, string> Abbreviations)
+        where TValue : INumberBase<TValue>, IComparisonOperators<TValue, TValue, bool>
+        where TInteger : INumberBase<TInteger>
+    {
+        return AbbreviateNumber(Value, 0, Abbreviations);
+    }
+    /// <inheritdoc cref="AbbreviateNumber{TValue, TAbbreviation}(TValue, int, IReadOnlyDictionary{TAbbreviation, string}, IFormatProvider?)"/>
+    public static string AbbreviateNumber<TValue>(this TValue Value, int DecimalPlaces = 0)
+        where TValue : INumberBase<TValue>, IComparisonOperators<TValue, TValue, bool>
+    {
+        return AbbreviateNumber(Value, DecimalPlaces, DefaultAbbreviations);
+    }
+}
